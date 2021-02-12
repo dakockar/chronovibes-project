e3174eea20743241d79ada3ea5bcacd99a1bf32d
Laura Heino <lhhein@gmail.com>
2021-02-12 14:07:02 +0100

    adds write view

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -92,16 +92,16 @@ router.post('/login', validateInput, (req, res, next) => {
       .then(isMatch => {
         if (isMatch) {
           req.session.loggedInUser = result
-          res.redirect('SOMEVIEW-HERE')
+          res.redirect('/home')
         }
         else {
-          res.render('LOGINVIEW-HERE', {msg: 'incorrect password'})
+          res.render('index', {msg: 'incorrect password'})
         }
       })
     }
     else {
       // username doesn't exist
-      res.render('LOGINVIEW-HERE', {msg: 'Username not found'})
+      res.render('index', {msg: 'Username not found'})
     }
   })
   .catch(err => console.log('error', err))
@@ -109,6 +109,9 @@ router.post('/login', validateInput, (req, res, next) => {
 
 
 // PROTECTED ROUTES
+
+
+
 
 
 module.exports = router
---
+++ routes/entry.routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const User = require('../models/User.model.js')
+
+
+router.get('/write', (req, res) => {
+  res.render()
+})
+
+
+router.post('/createpost', (req, res) => {
+
+})
+
+module.exports = router
