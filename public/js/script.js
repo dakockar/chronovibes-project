f456a12506c5466d05a95b501860ee3c2aebe85e
Laura Heino <lhhein@gmail.com>
2021-02-13 16:58:17 +0100

    started adding calendar

--- public/js/script.js
+++ public/js/script.js
@@ -7,11 +7,11 @@ document.addEventListener(
 );
 
 
-getQuote()
 
-// add randomized quote to home page
+// add randomized quote to home page 
+// TODO: view specific functions might have to be defined directly in the view files inside script tags
 
-async function getQuote() {
+async function getQuote(elem) {
     const rand = Math.floor(Math.random() * 1500)
     const result = await fetch('https://type.fit/api/quotes')
         
@@ -22,7 +22,49 @@ async function getQuote() {
         quote: data[rand].text
     }
 
-    document.querySelector('#quote').innerHTML= `${quote.quote} <br>- ${quote.author}`
+    elem.innerHTML= `${quote.quote} <br>- ${quote.author}`
 
     }
-        
+
+
+// generate a calendar for the month passed as args
+// TODO add links to specific days inside the td elements
+
+function createCal(elem, year, month) {
+      let m = month - 1 // get month number in js
+      let d = new Date(year, m) // get first day of given month
+  
+      let table = '<table><tr><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th><th>Sun</th></tr><tr>'
+  
+      // empty cells
+      for (let i = 0; i < getDay(d); i++) {
+          table += '<td></td>'
+      }
+  
+      // filled cells
+      while (d.getMonth() == m ) {
+          table += `<td>${d.getDate()}</td>`
+  
+          if (getDay(d) % 7 == 6) { // gets to last day of week
+              table += '<tr></tr>'
+          }
+          d.setDate(d.getDate() + 1)
+      }
+  
+      if (getDay(d) != 0) {
+          for (let i = getDay(d); i < 7; i++) {
+              table += '<td></td>'
+          }
+      }
+  
+      table += '</tr></table>'
+      elem.innerHTML = table
+  
+  }
+  
+
+function getDay(date) {
+    let day = date.getDate()
+    if (day == 0) day = 7
+    return day - 1
+  }
